a9699eee2208fd05758faeb298e8f80027fe6026
eamon <2460574924@qq.com>
2021-11-01 22:32:37 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    home组件
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.removeItem('token')
+      this.$router.replace('/login')
+    },
+  },
+}
+</script>
+
+<style></style>
