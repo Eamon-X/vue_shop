ed1f64060a4e2a5dfe7a6b33b2cb26f332dd447d
eamon <2460574924@qq.com>
2021-11-03 16:19:03 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -8,8 +8,16 @@ import '@/assets/css/global.css'
 import '@/assets/fonts/iconfont.css'
 //导入axios
 import axios from 'axios'
+
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//axios请求拦截
+axios.interceptors.request.use((config) => {
+  //为请求头对象，添加Token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  //必须返回config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
